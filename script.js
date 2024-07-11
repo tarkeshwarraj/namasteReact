47f481ebdb1e3f829fafd18b4a311c7cf0f8b60b
tarkeshwarraj <tarkeshwarraj@gmail.com>
2024-07-11 09:31:15 +0530

    episode-01

---
+++ script.js
@@ -0,0 +1,9 @@
+const heading = React.createElement(
+  "hi",
+  { id: "heading", className: "head" },
+  "This is first Rakesh React"
+);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(heading);
